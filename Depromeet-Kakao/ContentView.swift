bb76c0b8f4c6a861202692efa01ee1652d3ed245
ywonchae1 <ywonchae62@gmail.com>
2024-06-28 22:27:07 +0900

    Initial Commit

---
+++ Depromeet-Kakao/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Depromeet-Kakao
+//
+//  Created by 양원채 on 6/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Depromeet-Kakao/Depromeet_KakaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Depromeet_KakaoApp.swift
+//  Depromeet-Kakao
+//
+//  Created by 양원채 on 6/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Depromeet_KakaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
